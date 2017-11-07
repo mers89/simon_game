d16260cab5bae8a78156afed021a46753a33c992
Mercy Manrique <mercymanrique@yahoo.com>
2017-11-07 15:15:48 -0800

    update index.js

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const GREEN = "GREEN";
 
 var simon = {
  sendColor: function(color){
-  console.log("NEW COLOR: " + color);
+  console.log("NEW COLOR: " + color); 
  },
+ sequence: [],
+ colors: [RED, BLUE, YELLOW, GREEN],
+ nextSequence: function(){
+  var nextColor =simon.colors[Math.floor( Math.random() * simon.colors.length )];
+  console.log("the random color is ", nextColor);
+ }
 };
+
 $(document).ready(function(){
  $("#red").click(function(){ simon.sendColor(RED)});
  $("#blue").click(function(){ simon.sendColor(BLUE)});
  $("#yellow").click(function(){ simon.sendColor(YELLOW)});
  $("#green").click(function(){ simon.sendColor(GREEN)}); 
 })
+
+/* 
+1. Red
+2. Red, Blue
+3. Red, Blue, Yellow
+*/
