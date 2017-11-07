7b375e399832156acbcec6effeab36038ac4cbe6
Mercy Manrique <mercymanrique@yahoo.com>
2017-11-06 22:59:48 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const RED = "RED";
+const BLUE = "BLUE";
+const YELLOW = "YELLOW";
+const GREEN = "GREEN";
+
+var simon = {
+ sendColor: function(color){
+  console.log("NEW COLOR: " + color);
+ },
+};
+$(document).ready(function(){
+ $("#red").click(function(){ simon.sendColor(RED)});
+ $("#blue").click(function(){ simon.sendColor(BLUE)});
+ $("#yellow").click(function(){ simon.sendColor(YELLOW)});
+ $("#green").click(function(){ simon.sendColor(GREEN)}); 
+})
